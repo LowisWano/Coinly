3fc6a0cdb3a1a1247c126119c045af76f3cf91c3
luisandreiouano <110468848+LowisWano@users.noreply.github.com>
2025-05-05 20:52:52 +0800

    feat: add alnino's work transaction history feature

--- app/src/main/java/com/example/coinly/TransactionAdapter.java
+++ app/src/main/java/com/example/coinly/TransactionAdapter.java
@@ -1,5 +1,7 @@
 package com.example.coinly;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import java.util.List;
 
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.ViewHolder> {
     private List<Transaction> transactions;
+    private Context context;
 
     public TransactionAdapter(List<Transaction> transactions) {
         this.transactions = transactions;
@@ -18,7 +21,8 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
+        this.context = parent.getContext();
+        View view = LayoutInflater.from(context)
                 .inflate(R.layout.item_transaction, parent, false);
         return new ViewHolder(view);
     }
@@ -26,12 +30,23 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Transaction transaction = transactions.get(position);
-        
+
         holder.titleText.setText(transaction.getTitle());
         holder.dateText.setText(transaction.getDate());
         holder.amountText.setText(transaction.getFormattedAmount());
         holder.amountText.setTextColor(transaction.getAmountColor());
         holder.referenceText.setText("Ref Number: " + transaction.getRefNumber());
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, TransactionDetailsActivity.class);
+            intent.putExtra(TransactionDetailsActivity.EXTRA_TITLE, transaction.getTitle());
+            intent.putExtra(TransactionDetailsActivity.EXTRA_DATE, transaction.getDate());
+            intent.putExtra(TransactionDetailsActivity.EXTRA_AMOUNT, transaction.getAmount());
+            intent.putExtra(TransactionDetailsActivity.EXTRA_REFERENCE, transaction.getRefNumber());
+            intent.putExtra(TransactionDetailsActivity.EXTRA_SENDER, transaction.getSender());
+            intent.putExtra(TransactionDetailsActivity.EXTRA_RECEIVER, transaction.getReceiver());
+            context.startActivity(intent);
+        });
     }
 
     @Override
