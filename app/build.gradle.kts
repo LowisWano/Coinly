efa1b58fb85439ce09b894fcb6a56b8db21c502f
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-05-13 23:08:33 +0800

    feat: add `navigationUi` and `navigationFragment` library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
+
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
 }
