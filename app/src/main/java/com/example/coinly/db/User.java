42d4146079c65e613360c519baa053328ef3d3c4
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-05-13 20:34:59 +0800

    feat: add `signUp` and `login` function call

--- app/src/main/java/com/example/coinly/db/User.java
+++ app/src/main/java/com/example/coinly/db/User.java
@@ -1,6 +1,9 @@
 package com.example.coinly.db;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.Date;
+import java.util.Map;
 
 public class User {
     public static class Credentials {
@@ -35,5 +38,48 @@ public class User {
         String name;
         float target;
         float balance;
+    }
+
+    public static void signUp(Credentials credentials, Database.ID callback) {
+        Map<String, Object> user = Map.of(
+                "email", credentials.email,
+                "password", credentials.password, // TODO: Encrypt the password
+                "pin", credentials.pin
+        );
+
+        FirebaseFirestore db = Database.db();
+
+        db.collection("users")
+                .whereEqualTo("email", credentials.email)
+                .get()
+                .addOnSuccessListener(querySnapshot -> {
+                    if (!querySnapshot.isEmpty()) {
+                        callback.onFailure(new Database.KeyAlreadyExists("Email already exists"));
+                        return;
+                    }
+
+                    db.collection("users")
+                            .add(user)
+                            .addOnSuccessListener(doc -> callback.onSuccess(doc.getId()))
+                            .addOnFailureListener(callback::onFailure);
+                })
+                .addOnFailureListener(callback::onFailure);
+    }
+
+    public static void login(Credentials credentials, Database.ID callback) {
+        Database.db().collection("users")
+                .whereEqualTo("email", credentials.email)
+                .whereEqualTo("password", credentials.password) // TODO: Change to use decryption
+                .get()
+                .addOnSuccessListener(querySnapshot -> {
+                    if (querySnapshot.isEmpty()) {
+                        callback.onFailure(new Database.DataNotFound("Email not found"));
+                    }
+
+                    // TODO: Implement here the decryption
+                    callback.onSuccess(querySnapshot.getDocuments().get(0).getId());
+                })
+                .addOnFailureListener(callback::onFailure);
+
     }
 }
