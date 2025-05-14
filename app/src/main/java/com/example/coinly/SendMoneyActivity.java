8b7a0f220f90a0e7570b2f9d66a01f3762416def
Dempsey <dagantareynat@gmail.com>
2025-05-15 04:09:58 +0800

    feat: integrate backend logic

--- app/src/main/java/com/example/coinly/SendMoneyActivity.java
+++ app/src/main/java/com/example/coinly/SendMoneyActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coinly.db.Database;
+import com.example.coinly.db.User;
 
 public class SendMoneyActivity extends AppCompatActivity {
 
@@ -29,9 +34,6 @@ public class SendMoneyActivity extends AppCompatActivity {
         editTextNumber = findViewById(R.id.editTextNumber);
         editTextAmount = findViewById(R.id.editTextAmount);
         nextButton = findViewById(R.id.nextButton);
-
-        // Disable Next button initially
-        nextButton.setEnabled(false);
 
         backArrow.setOnClickListener(v -> {
             Intent intent = new Intent(SendMoneyActivity.this, MainActivity.class);
@@ -68,6 +70,8 @@ public class SendMoneyActivity extends AppCompatActivity {
         editTextNumber.addTextChangedListener(formWatcher);
         editTextAmount.addTextChangedListener(formWatcher);
 
+        String UserID = getSharedPreferences("coinly", MODE_PRIVATE).getString("userId", "");
+
         // Handle "Next" button click
         nextButton.setOnClickListener(v -> {
             String number = editTextNumber.getText().toString().trim();
@@ -76,18 +80,69 @@ public class SendMoneyActivity extends AppCompatActivity {
             if (number.isEmpty() || amount.isEmpty()) {
                 Toast.makeText(SendMoneyActivity.this, "Please enter both number and amount", Toast.LENGTH_SHORT).show();
             } else {
-                Intent intent = new Intent(SendMoneyActivity.this, SendMoneyConfirmActivity.class);
-                intent.putExtra("number", number);
-                intent.putExtra("amount", amount);
-                startActivity(intent);
+
             }
+
+            User.sendMoneyFromPhoneNumber(UserID, number, Float.parseFloat(amount), new Database.Data<String>() {
+                @Override
+                public void onSuccess(String data) {
+                    Intent intent = new Intent(SendMoneyActivity.this, SendMoneyConfirmActivity.class);
+                    intent.putExtra("number", number);
+                    intent.putExtra("amount", amount);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onFailure(Exception e) {
+                    Log.e("Error", "Error", e);
+                }
+            });
         });
+
+
+
+        User.get(
+                UserID,
+                User.Details.class,
+                new Database.Data<User.Details>(){
+
+                    @Override
+                    public void onSuccess(User.Details data) {
+                        TextView userName, phoneNumber;
+                        userName = findViewById(R.id.userName);
+                        phoneNumber = findViewById(R.id.phoneNumber);
+
+                        userName.setText(data.fullName.first + data.fullName.middleInitial + data.fullName.last);
+                        phoneNumber.setText(data.phoneNumber);
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        // invalid phone number, invalid amount
+                    }
+                }
+        );
+
+        User.get(
+                UserID,
+                User.Wallet.class,
+                new Database.Data<User.Wallet>(){
+
+                    @Override
+                    public void onSuccess(User.Wallet data) {
+                        TextView enterAmount = findViewById(R.id.enterAmount);
+
+                        enterAmount.setText(String.format("Enter Amount (%.2f)", data.balance));
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.e("Error", "Error", e);
+                    }
+                }
+        );
 
     }
 
-    // Placeholder validation function
-    // private boolean validateNumberWithDatabase(String number) {
-    //     // TODO: Query your Firestore or Realtime DB here
-    //     return true;
-    // }
+
 }
